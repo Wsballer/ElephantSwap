2d8486282a9d0e34fa3feb18dd216f9e868728f7
Tunji Oyewole <t.oyewole23@outlook.com>
2021-06-08 16:47:12 -0400

    Add project files.

---
+++ ElephantSwap/Elephant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElephantSwap
+{
+    class Elephant
+    {
+        public int EarSize;
+        public string Name;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is {0}.", Name);
+            Console.WriteLine("My ears are {0} inches tall.", EarSize);
+        }
+    }
+}
---
+++ ElephantSwap/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ElephantSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    Console.WriteLine("You pressed 1\nCalling lloyd.WhoAmI()");
+                    lloyd.WhoAmI();
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("You pressed 2\nCalling lucinda.WhoAmI()");
+                    lucinda.WhoAmI();
+                }
+                else if (input == "3")
+                {
+                    Console.WriteLine("You pressed 3\nReferences have been swapped");
+                    Elephant holder;
+                    holder = lucinda;
+                    lucinda = lloyd;
+                    lloyd = holder;
+                }
+                else return;
+                Console.WriteLine();
+            }
+        }
+    }
+}
